74913216f9eaba37d5fcf5397826ded68d179292
theo82 <theoaristi53@gmail.com>
2020-11-12 11:43:01 +0200

    Splitting the App Into Components

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Button, TextInput, View, Text, FlatList } from 'react-native';
+import GoalItem from './components/GoalItem';
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState('');
@@ -31,9 +32,7 @@ export default function App() {
         <FlatList 
           data={courseGoals} 
           renderItem={itemData => (
-            <View style={styles.listItem}>
-              <Text>{itemData.item.value}</Text>
-            </View>
+            <GoalItem title={itemData.item.value} />
            )} 
         />
     </View>
@@ -54,12 +53,5 @@ const styles = StyleSheet.create({
     borderColor: 'black', 
     borderWidth: 1, 
     padding: 10
-  },
-  listItem: {
-    padding: 10,
-    marginVertical: 10,
-    backgroundColor: '#ccc',
-    borderColor: 'black',
-    borderWidth: 1
   }
 });
